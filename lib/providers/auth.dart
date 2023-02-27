b35a545caeb86f0ba6b8d224a8b5e297d843ae19
Vaibhav77e <chandrashekharvaibhav@gmail.com>
2023-02-27 22:08:26 +0530

    added auth _screen

---
+++ lib/providers/auth.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier {
+  String? _token;
+  DateTime? _expireDate;
+  String? _userId;
+
+  Future<void> signup(String email, String password) async {
+    // fill your google api key
+    final url = Uri.parse('');
+
+    var response = await http.post(url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }));
+    print(json.decode(response.body));
+  }
+}
